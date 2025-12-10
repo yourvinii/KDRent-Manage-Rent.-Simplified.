54d9a8f40c1858335a05372d0145f3c3c086e5c3
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-10 12:34:40 +0530

    feat(config): Added database configuration setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ DB Connected");
+  } catch (error) {
+    console.log("❌ Error connecting DB : ", error);
+  }
+};
+
+export default connectDB;
