05013ced771acb63641d640fe87eb316f808c802
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-10 12:33:18 +0530

    refactor(app): Separated Express app configuration from server startup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,21 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+// // routes
+
+// /* ---------- Global Middlewares ---------- */
+app.use(cors());
+app.use(express.json()); // to read JSON body
+app.use(express.urlencoded({ extended: true }));
+
+// /* ---------- Routes ---------- */
+
+// /* ---------- Health Check ---------- */
+app.get("/data", (req, res) => {
+  return res
+    .status(500)
+    .json({ success: true, message: "KDRent API running 🚀" });
+});
 
 export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,23 @@
 import app from "./app.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
-app.listen(8080,()=>{
-  console.log("Server is listening on port 8080")
-})
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT || 5000;
+
+// /* ---------- Start Server ---------- */
+const startServer = async () => {
+  try {
+    
+    app.listen(PORT, () => {
+      console.log(`✅ Server is running on port ${PORT}`);
+    });
+    await connectDB();
+  } catch (error) {
+    console.log("❌ Server Failed to start", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
