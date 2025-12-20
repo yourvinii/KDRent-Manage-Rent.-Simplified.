5a22eb861b022dbfdebe03e87864c0b121c0f403
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-20 20:28:20 +0530

    chore(frontend): Setup client-side routing in router.jsx

---
+++ frontend/src/router.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "./layout/MainLayout.jsx";
+import Landing from "./pages/Landing/Landing.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+
+import DashboardLayout from "./layout/DashboardLayout.jsx";
+import AddProperty from "./pages/Dashboard/AddProperty.jsx";
+import MyProperties from "./pages/Dashboard/MyProperties.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "add-property", element: <AddProperty /> },
+      { path: "my-properties", element: <MyProperties /> },
+    ],
+  },
+]);
+
+export default router;
