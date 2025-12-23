539409587810ee4500d719b533301d37bd52cb9b
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-24 00:50:47 +0530

    chore(format): Adjusted formatting in authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import authController from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
+router.post("/register", authController.signup);
 router.post("/login", authController.login);
 
 export default router;
