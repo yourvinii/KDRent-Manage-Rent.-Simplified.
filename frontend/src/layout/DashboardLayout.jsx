575bcdce02e9ac1b155eda3802600292ae3c0b69
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-24 00:53:15 +0530

    feat(auth): Integrated AuthContext into DashboardLayout

--- frontend/src/layout/DashboardLayout.jsx
+++ frontend/src/layout/DashboardLayout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
 import DashboardNav from "../components/DashboardNav.jsx";
+
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext.jsx";
+
 export default function DashboardLayout() {
+  const { user } = useContext(AuthContext);
   return (
     <>
       <h1>Owner Dashboard</h1>
-      <DashboardNav/>
+      <p>{user.name} is logged in</p>
+      <DashboardNav />
       <Outlet />
     </>
   );
