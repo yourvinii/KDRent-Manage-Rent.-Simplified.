8a35c26bafd2eb019773def019ac4f4b7d24438a
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-24 17:26:04 +0530

    Remove the test thing

--- frontend/src/layout/DashboardLayout.jsx
+++ frontend/src/layout/DashboardLayout.jsx
@@ -5,11 +5,11 @@ import { useContext } from "react";
 import { AuthContext } from "../context/authContext.jsx";
 
 export default function DashboardLayout() {
-  const { user } = useContext(AuthContext);
+
   return (
     <>
       <h1>Owner Dashboard</h1>
-      <p>{user.name} is logged in</p>
+      
       <DashboardNav />
       <Outlet />
     </>
