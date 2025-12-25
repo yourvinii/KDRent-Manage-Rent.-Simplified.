f5640bff64eae74715f4897e4571d96a3a8b2c34
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-25 13:21:58 +0530

    chore(frontend): Created PrivateRoute component for authentication-based routing

---
+++ frontend/src/routes/PrivateRoutes.jsx
@@ -0,0 +1,13 @@
+import {  useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { token } = useContext(AuthContext);
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+export default PrivateRoute;
