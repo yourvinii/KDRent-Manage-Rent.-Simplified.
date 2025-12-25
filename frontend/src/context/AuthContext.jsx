b595ac0602723abd063b689b00fc40aab1e94242
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-12-25 13:17:22 +0530

    chore(frontend): Setup AuthContext skeleton for global auth state

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,59 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { loginUser as loginService } from "../services/authService";
+import Cookies from "js-cookie";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
-export default function AuthProvider({ children }) {
-  const user = {
-    id: 1,
-    name: "Vinayak",
+const AuthProvider = ({ children }) => {
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  //Login
+  // const login = async (credentials) => {
+  //   await authService.login(credentials);
+  //   //if API succeeds, cookie is already set
+  //   setIsAuthenticated(true);
+  // };
+
+  const loginUser = async (data) => {
+    try {
+      const response = await loginService(data);
+
+      //assuming backend sends: {user, token}
+      Cookies.set("token", response.token);
+      setToken(response.token);
+      setUser(response.user);
+
+      return response;
+    } catch (error) {
+      throw error;
+    }
   };
+
+  // //logout
+  // const logout = async () => {
+  //   await authService.logout();
+  //   setIsAuthenticated(false);
+  // };
+
+  const logoutUser = () => {
+    Cookies.remove("token");
+    setToken(null);
+    setUser(null);
+  };
+
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider
+      value={{
+        user,
+        token,
+        loginUser,
+        logoutUser,
+      }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
+
+export { AuthContext, AuthProvider };
