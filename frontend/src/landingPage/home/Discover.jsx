af4eca176d049b252df601be6117df455919ebfe
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-03-12 21:11:16 +0530

    Discover Page

--- frontend/src/landingPage/home/Discover.jsx
+++ frontend/src/landingPage/home/Discover.jsx
@@ -1,3 +1,16 @@
-export default function Discover(){
-    return <h1>Discover Page</h1>
+import DiscoverCard from "../../cards/DiscoverCard";
+const data =[{
+    
+}]
+
+export default function Discover() {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-4"><DiscoverCard/></div>
+            <div className="col-4"><DiscoverCard/></div>
+            <div className="col-4"><DiscoverCard/></div>
+        </div>
+    </div>
+  )
 }
