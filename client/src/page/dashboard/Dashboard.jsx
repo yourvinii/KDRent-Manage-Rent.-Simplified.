0e1323c199d3d739573ea13cbc9d109463ce9653
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-03-31 12:14:10 +0530

    refactor: dashboard

--- client/src/page/dashboard/Dashboard.jsx
+++ client/src/page/dashboard/Dashboard.jsx
@@ -1,3 +1,26 @@
-export default function Dashboard(){
-  return <div>Dashboard</div>
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
+
+export default function Dashboard() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/login");
+  };
+  return (
+    <div className="container">
+      <h1>dashboard</h1>
+      <div className="row">
+        <div className="col-4">
+          <h1>side panel</h1>
+          <NavLink to={"/dashboard/my-properties"}>My Properties</NavLink>
+          <NavLink to={"/dashboard/add-property"}>Add Property</NavLink>
+          <button onClick={handleLogout}>logout</button>
+        </div>
+        <div className="col-8">main container</div>
+      </div>
+    </div>
+  );
 }
