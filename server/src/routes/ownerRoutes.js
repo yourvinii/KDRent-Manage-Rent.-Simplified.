f597089f24cff1a2adc450cb7d13132265f3c43d
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-05-25 17:47:02 +0530

    Delete frontend

--- server/src/routes/ownerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import verifyToken from "../middleware/authMiddleware.js";
-import ownerController from "../controllers/ownerController.js";
-
-router.get("/dashboard", verifyToken, ownerController.ownerDashboard);
-
-export default router;
