4b9f40a16e8f324f63b52d2f82734e6c32057a44
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-05-25 17:36:21 +0530

    chore: frontend setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./router.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
-);
+)
--- client/src/services/api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:8080/api",
-  withCredentials: true,
-});
-
-export default API;
