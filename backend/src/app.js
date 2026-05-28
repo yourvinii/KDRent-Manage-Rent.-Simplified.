6e14390ced4d4d5f77e3c73401be76cee42a569d
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-05-28 21:24:23 +0530

    refactor CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,18 +25,20 @@ const allowedOrigins = (process.env.CLIENT_URL || "http://localhost:5173")
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   }),
+// );
 
 // routes
 app.get("/", (req, res) => {
